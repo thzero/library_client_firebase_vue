e8ab129dad0fb310e9f48d2b504a5b1f71f456ba
thzero <thzero@users.noreply.github.com>
2023-11-10 16:10:54 -0600

    updates

--- boot/starter.js
+++ boot/starter.js
@@ -1,47 +1,54 @@
-// import starter from '@thzero/library_client_firebase/boot/starter';
+import LibraryClientConstants from '@thzero/library_client/constants';
 
-// // export default async ({
-// export default ({
-// 	router
-// }) => {
-// 	return starter(() => {
-// 		router.beforeResolve(async (to, from, next) => {
-// 			const correlationId = LibraryCommonUtility.correlationId();
-// 			serviceLogger.debug('middleware', 'router.beforeResolve', null, to, correlationId);
-// 			if (!to.matched.some(record => record.meta.requiresAuth)) {
-// 				next();
-// 				return;
-// 			}
+import LibraryClientUtility from '@thzero/library_client/utility/index';
+import LibraryCommonUtility from '@thzero/library_common/utility';
 
-// 			const record = to.matched.find(record => record.meta.requiresAuth);
-// 			let requiresAuthRoles = [];
-// 			let requiresAuthLogical = null;
-// 			console.log('authorization.record', record);
-// 			if (record && record.meta) {
-// 				console.log('authorization.record.meta', record.meta);
-// 				requiresAuthRoles = (record.meta.requiresAuthRoles && Array.isArray(record.meta.requiresAuthRoles)) ? record.meta.requiresAuthRoles : [];
-// 				serviceLogger.info2('authorization.roles', requiresAuthRoles);
-// 				console.log('authorization.roles', requiresAuthRoles);
-// 				requiresAuthLogical = (record.meta.requiresAuthRoles && Array.isArray(record.meta.requiresAuthLogical)) ? record.meta.requiresAuthLogical : null;
-// 				serviceLogger.info2('authorization.logical', requiresAuthLogical);
-// 				console.log('authorization.logical', requiresAuthLogical);
-// 			}
+import starter from '@thzero/library_client_firebase/boot/starter';
 
-// 			const serviceAuth = LibraryClientUtility.$injector.getService(LibraryClientConstants.InjectorKeys.SERVICE_AUTH);
-// 			const result = await serviceAuth.resolveAuthorization(correlationId, requiresAuthRoles, requiresAuthLogical);
-// 			serviceLogger.info2('authorization.result', result);
-// 			console.log('authorization.result', result);
-// 			if (!result) {
-// 				LibraryClientUtility.$navRouter.push('/', null, () => {
-// 					// LibraryClientUtility.$navRouter.push('/')
-// 					// window.location.href = '/'
-// 				});
-// 				return;
-// 			}
+// export default async ({
+export default async ({router}) => {
+	const serviceLogger = LibraryClientUtility.$injector.getService(LibraryClientConstants.InjectorKeys.SERVICE_LOGGER);
 
-// 			serviceLogger.info2('authorization - success');
-// 			console.log('authorization - success');
-// 			next();
-// 		});
-// 	});
-// }
+    router.beforeResolve(async (to, from, next) => {
+        const correlationId = LibraryCommonUtility.correlationId();
+        serviceLogger.debug('middleware', 'router.beforeResolve', null, to, correlationId);
+        if (!to.matched.some(record => record.meta.requiresAuth)) {
+            next();
+            return;
+        }
+
+        const record = to.matched.find(record => record.meta.requiresAuth);
+        let requiresAuthRoles = [];
+        let requiresAuthLogical = null;
+        // console.log('authorization.record', record);
+        serviceLogger.debug('starter', 'beforeResolve', 'authorization.record', record, correlationId);
+        if (record && record.meta) {
+            // console.log('authorization.record.meta', record.meta);
+			serviceLogger.debug('starter', 'beforeResolve', 'authorization.record.meta', record.meta, correlationId);
+            requiresAuthRoles = (record.meta.requiresAuthRoles && Array.isArray(record.meta.requiresAuthRoles)) ? record.meta.requiresAuthRoles : [];
+            // console.log('authorization.roles', requiresAuthRoles);
+			serviceLogger.debug('starter', 'beforeResolve', 'authorization.roles', requiresAuthRoles, correlationId);
+            requiresAuthLogical = (record.meta.requiresAuthRoles && Array.isArray(record.meta.requiresAuthLogical)) ? record.meta.requiresAuthLogical : null;
+            // console.log('authorization.logical', requiresAuthLogical);
+			serviceLogger.debug('starter', 'beforeResolve', 'authorization.logical', requiresAuthLogical, correlationId);
+        }
+
+        const serviceAuth = LibraryClientUtility.$injector.getService(LibraryClientConstants.InjectorKeys.SERVICE_AUTH);
+        const result = await serviceAuth.resolveAuthorization(correlationId, requiresAuthRoles, requiresAuthLogical);
+        // console.log('authorization.result', result);
+        serviceLogger.debug('starter', 'beforeResolve', 'authorization.result', result, correlationId);
+        if (!result) {
+            LibraryClientUtility.$navRouter.push('/', null, () => {
+                // LibraryClientUtility.$navRouter.push('/')
+                // window.location.href = '/'
+            });
+            return;
+        }
+
+        serviceLogger.info2('authorization - success');
+        // console.log('authorization - success');
+        next();
+    });
+
+	return await starter(router);
+}
